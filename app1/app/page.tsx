7058057deb8f99a359226bdb30b77ae8e33ec5cb
sniffowomo <123172291+sniffowomo@users.noreply.github.com>
2023-03-16 12:12:15 +0000

    🆕:🍥

--- app1/app/page.tsx
+++ app1/app/page.tsx
@@ -13,12 +13,12 @@ export default function Home() {
       <h1>Salty Champagne🍹</h1>
       <Image src={sw} alt="sweatydrink" width={800} />
       <p style={{ color: "Pink", fontSize: "40px" }}>
-        Ennoy the strong stinks "How bad do you want this sweaty pussy," she
-        said as she started to rub her clit through her spandex. "Or maybe it is
-        my sweaty ass that you want?" Which she followed up by her reaching in
-        the back of her shorts, ran her finger up her sweaty crack and rubbed
-        her finger under my nose. It smelled so sweaty and musty but in an
-        amazing way.
+        Ennoy the strong stinks How bad do you want this sweaty pussy, she said
+        as she started to rub her clit through her spandex. Or maybe it is my
+        sweaty ass that you want? Which she followed up by her reaching in the
+        back of her shorts, ran her finger up her sweaty crack and rubbed her
+        finger under my nose. It smelled so sweaty and musty but in an amazing
+        way.
       </p>
     </main>
   );
