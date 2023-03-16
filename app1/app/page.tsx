115b7b94f401902becd418f1ecd91fcb17182522
sniffowomo <123172291+sniffowomo@users.noreply.github.com>
2023-03-16 09:07:40 +0000

    🆕:🍥

--- app1/app/page.tsx
+++ app1/app/page.tsx
@@ -2,13 +2,17 @@ import Image from "next/image";
 import { Inter } from "next/font/google";
 import styles from "./page.module.css";
 
+// Image Import
+import sw from "w.gif";
+
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
     <main className={styles.main}>
       <h1>Howdy Doody</h1>
+      <Image src={sw} alt="sweatydrink" />
       <p>"yahahhahahah" "hohohohoh"</p>
-    </main>
+    </main>  
   );
 }
